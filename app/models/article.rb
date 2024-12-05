dfc081b826a22199a8bf8101ea928a06292c52de
Manish Patel <patelm.2280@gmail.com>
2024-12-05 11:46:44 +0530

    more updates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
     # validates :title, absence: true
     # validates_absence_of :title, message: "title should not be set manually"
+    has_rich_text :body
 end
